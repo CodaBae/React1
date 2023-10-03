adb5946c5236ea9787737b9e21ca8fd4f723c4d9
Shalom Ijeoma Mathew <codabae@Shaloms-MacBook-Pro-2.local>
2023-10-03 08:44:44 +0100

    class 1

---
+++ Class2/intro/src/components/Greeting.jsx
@@ -0,0 +1,10 @@
+const Greeting = () => {
+    return (
+        <div>
+            <h1>Hello, Lily</h1>
+            <button>Click here to get started</button>
+        </div>
+    )
+}
+
+export default Greeting
