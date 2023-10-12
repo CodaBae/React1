8181dd97741fb98683ccdd6d93a21723ca2140d0
Shalom Mathew <38937931+CodaBae@users.noreply.github.com>
2023-10-12 23:00:11 +0100

    class 5

---
+++ Class5/intro/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <>
+            <h1>To Do App</h1>
+        </>
+    )
+}
+
+export default Header
---
+++ Class5/intro/src/pages/Todo.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header"
+import '../styles/Todo.css'
+
+const Todo = () => {
+    return (
+        <div id="todo_container">
+           <Header />
+           
+        </div>
+    )
+}
+
+export default Todo
